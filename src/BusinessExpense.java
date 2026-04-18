7213283350dae3f9caca4ec71e7a574fb895e43c
esmasamm <twenty2one1kg@gmail.com>
2026-04-18 22:56:10 +0600

    1

--- src/BusinessExpense.java
+++ src/BusinessExpense.java
@@ -1,2 +1,14 @@
-public class BusinessExpense {
+public class BusinessExpense extends Expense {
+    private String company;
+
+    public BusinessExpense(int id, double amount, String category, String company) {
+        super(id, amount, category);
+        this.company = company;
+    }
+
+    @Override
+    public void display() {
+        System.out.println(getId() + " | " + getAmount() + " | " +
+                getCategory() + " | " + company);
+    }
 }
